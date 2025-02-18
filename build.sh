72c318b644c1b390c710d54eda8a83d8b17d1ab3
Nathan <ndowens@artixlinux.org>
2025-02-18 11:53:49 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y hyprland xdg-desktop-portal-hyprland hypridle hyprcursor hyprlock hyprutils nwg-panel mosh
+for i in hyprland xdg-desktop-portal-hyprland hypridle hyprcursor hyprlock hyprutils nwg-panel mosh ; do
+  dnf install -y $i ;
+done
 
 # Use a COPR Example:
 #
