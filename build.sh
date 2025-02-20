54a78dfd104a617a96655ef26e60166358134247
Nathan <ndowens@artixlinux.org>
2025-02-20 04:22:12 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y hyprland xdg-desktop-portal-hyprland hypridle hyprcursor hyprlock hyprutils nwg-panel mosh @kde-desktop plasma-workspace-x11 @base-x
+dnf5 install -y hyprland xdg-desktop-portal-hyprland hypridle hyprcursor hyprlock hyprutils nwg-panel mosh @kde-desktop plasma-workspace-x11 @base-x kitty
 dnf5 remove -y xorg-x11-drv-nouveau
 
 #Add username
